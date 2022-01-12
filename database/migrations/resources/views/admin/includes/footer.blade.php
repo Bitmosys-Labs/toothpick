57790648a96ed79fa0d2ef8bfbe16599381252bb
pratikdahal105 <31059959+pratikdahal105@users.noreply.github.com>
2022-01-12 20:00:48 +0545

    database

--- database/migrations/resources/views/admin/includes/footer.blade.php
+++
@@ -1,6 +0,0 @@
-<footer class="site-footer">
-    <div class="site-footer-legal">© {{ date('Y') }} <a href="">EBV</a></div>
-    <div class="site-footer-right">
-      Exclusive Balli Villa
-    </div>
-</footer>
