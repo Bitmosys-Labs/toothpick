778ec634fcfbd9715cc4b4cc64ba49f8f7980f53
Pratik Dahal <pratikdahal105@gmail.com>
2022-03-04 19:12:22 +0545

    final change in login

--- app/Http/Controllers/Api/UserApiController.php
+++ app/Http/Controllers/Api/UserApiController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Core_modules\User\Model\User;
 use App\Http\Controllers\Controller;
 use App\Modules\Dcp\Model\Dcp;
+use App\Modules\Practice\Model\Practice;
 use App\Modules\Staff\Model\Staff;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
@@ -17,59 +18,72 @@ class UserApiController extends Controller
     }
 
     public function registerUser(Request $request){
-        // if(User::where('email', '=', $request->email)->whereNotNull('email_verified_at')->exists()) {
-        //     return response('Email already exists!');
-        // }
-        // $user = new User();
-        // if($request->role == 1){
-        //     $request->validate([
-        //         'full_name' => 'required',
-        //         'email' => 'required',
-        //         'contact' => 'required',
-        //         'password' => 'required|min:6',
-        //         'confirm_password' => 'required|min:6|same:password',
-        //         'role' => 'required',
-        //     ]);
-        //     $data = [
-        //         'name' => $request->fullname,
-        //         'email' => $request->email,
-        //         'contact' => $request->contact,
-        //         'password' => Hash::make($request->password),
-        //         'role' => $request->role,
-        //     ];
-        //     $newUser = $user->save($data);
-        //     return response('Success');
-        // }elseif ($request->role == 2){
-        //     $request->validate([
-        //         'full_name' => 'required',
-        //         'email' => 'required',
-        //         'contact' => 'required',
-        //         'password' => 'required|min:6',
-        //         'confirm_password' => 'required|min:6|same:password',
-        //         'role' => 'required',
-        //         'staff_id' => 'required',
-        //     ]);
-        //     $data = [
-        //         'name' => $request->fullname,
-        //         'email' => $request->email,
-        //         'contact' => $request->contact,
-        //         'password' => Hash::make($request->password),
-        //         'role' => $request->role,
-        //     ];
-        //     $newUser = $user->save($data);
-        //     $dcp = new Dcp();
-        //     $dcp_data = [
-        //         'user_id' => $newUser->id,
-        //         'staff_id' => $request->staff_id,
-        //     ];
-        //     try{
-        //         $dcp->save($dcp_data);
-        //         return response('Success');
-        //     }
-        //     catch (\Exception $e){
-        //         $user->where('id', $newUser->id)->delete();
-        //     }
-        // }
+//         if(User::where('email', '=', $request->email)->whereNotNull('email_verified_at')->exists()) {
+//             return response('Email already exists!');
+//         }
+//         $user = new User();
+//         if($request->role == 1){
+//             $request->validate([
+//                 'full_name' => 'required',
+//                 'email' => 'required',
+//                 'contact' => 'required',
+//                 'password' => 'required|min:6',
+//                 'confirm_password' => 'required|min:6|same:password',
+//                 'role' => 'required',
+//             ]);
+//             $data = [
+//                 'name' => $request->fullname,
+//                 'email' => $request->email,
+//                 'contact' => $request->contact,
+//                 'password' => Hash::make($request->password),
+//                 'role' => $request->role,
+//             ];
+//             $newUser = $user->save($data);
+//             $practice = new Practice();
+//             $practice_data = [
+//                 'user_id' => $newUser->id,
+//             ];
+//
+//             try{
+//                 $practice->save($practice_data);
+//                 return response('Success');
+//             }
+//             catch (\Exception $e){
+//                 $user->where('id', $newUser->id)->delete();
+//                 return response('Registration Failed!', 404);
+//             }
+//         }elseif ($request->role == 2){
+//             $request->validate([
+//                 'full_name' => 'required',
+//                 'email' => 'required',
+//                 'contact' => 'required',
+//                 'password' => 'required|min:6',
+//                 'confirm_password' => 'required|min:6|same:password',
+//                 'role' => 'required',
+//                 'staff_id' => 'required',
+//             ]);
+//             $data = [
+//                 'name' => $request->fullname,
+//                 'email' => $request->email,
+//                 'contact' => $request->contact,
+//                 'password' => Hash::make($request->password),
+//                 'role' => $request->role,
+//             ];
+//             $newUser = $user->save($data);
+//             $dcp = new Dcp();
+//             $dcp_data = [
+//                 'user_id' => $newUser->id,
+//                 'staff_id' => $request->staff_id,
+//             ];
+//             try{
+//                 $dcp->save($dcp_data);
+//                 return response('Success');
+//             }
+//             catch (\Exception $e){
+//                 $user->where('id', $newUser->id)->delete();
+//                 return response('Registration Failed!', 404);
+//             }
+//         }
 
         if($request->all()){
             return response('Success');
